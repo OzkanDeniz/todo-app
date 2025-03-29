eb1d9c75ff0214e7a889cbcb693578269d622fde
ozkan <ozkandnz09@gmail.com>
2025-03-29 23:37:41 +0300

    header main

---
+++ src/pages/Main.tsx
@@ -0,0 +1,12 @@
+import { Container } from "@mui/material";
+import Header from "../components/Header";
+
+const Main = () => {
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
+};
+
+export default Main;
