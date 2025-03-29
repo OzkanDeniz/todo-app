f479e9510ed776529636917430587f7fa65c5262
ozkan <ozkandnz09@gmail.com>
2025-03-29 23:45:33 +0300

    sx=inline style

---
+++ src/components/AddTodo.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const AddTodo = () => {
+  return (
+    <Box component="form" sx={{display:{xs:"block", sm:"flex"}, justifyContent:{}}}>
+      
+    </Box>
+  )
+}
+
+export default AddTodo
