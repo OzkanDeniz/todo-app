e049e7a15452203d3ff449810972b5481720ccfa
ozkan <ozkandnz09@gmail.com>
2025-03-29 23:57:47 +0300

    input button

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,10 +1,12 @@
 import { Container } from "@mui/material";
 import Header from "../components/Header";
+import AddTodo from "../components/AddTodo";
 
 const Main = () => {
   return (
     <Container>
       <Header />
+      <AddTodo/>
     </Container>
   );
 };
