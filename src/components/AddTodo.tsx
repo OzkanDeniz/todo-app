2e1b8a82e8f040e1a3700a3906f30febc2220a18
ozkan <ozkandnz09@gmail.com>
2025-03-30 00:01:14 +0300

    type

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,14 +1,34 @@
-import { Height, Save } from '@mui/icons-material'
-import { Box, Button, TextField } from '@mui/material'
-import React from 'react'
+import { Save } from "@mui/icons-material";
+import { Box, Button, TextField } from "@mui/material";
+import { useState } from "react";
 
 const AddTodo = () => {
+  const [task, setTask] = useState<string>("");
   return (
-    <Box component="form" sx={{display:{xs:"block", sm:"flex"}, justifyContent:{xs:"flex-start",sm:"center"},m:{xs:1, sm:"auto"}, height:{xs:"120px", sm:"80px"}}}>
-      <TextField variant='outlined' color='success' sx={{minWidth:{xs:"100%", sm:"50%"}, height:"50px", m:1}}/>
-      <Button variant="contained" endIcon={<Save />} color='success' sx={{minWidth:{xs:"100%", sm:"15%"},height:"55px", m:1}}>Save Todo</Button>
+    <Box
+      component="form"
+      sx={{
+        display: { xs: "block", sm: "flex" },
+        justifyContent: { xs: "flex-start", sm: "center" },
+        m: { xs: 1, sm: "auto" },
+        height: { xs: "120px", sm: "80px" },
+      }}
+    >
+      <TextField
+        variant="outlined"
+        color="success"
+        sx={{ minWidth: { xs: "100%", sm: "50%" }, height: "50px", m: 1 }}
+      />
+      <Button
+        variant="contained"
+        endIcon={<Save />}
+        color="success"
+        sx={{ minWidth: { xs: "100%", sm: "15%" }, height: "55px", m: 1 }}
+      >
+        Save Todo
+      </Button>
     </Box>
-  )
-}
+  );
+};
 
-export default AddTodo
+export default AddTodo;
