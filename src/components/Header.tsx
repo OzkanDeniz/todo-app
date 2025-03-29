f5ed82890e692edd994247c3c0018aae61e93efd
ozkan <ozkandnz09@gmail.com>
2025-03-29 23:40:24 +0300

    header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,7 @@
+import { Typography } from "@mui/material";
+
 const Header = () => {
-  return <div></div>;
+  return <Typography color="error" variant="h2" component="h1" align="center">Todo App With Typescript</Typography>;
 };
 
 export default Header;
