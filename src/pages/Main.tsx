1b00af38dd9e68862ff672dd4697914df6846eac
ozkan <ozkandnz09@gmail.com>
2025-03-30 13:27:09 +0300

    async await try catch axios

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -2,6 +2,7 @@ import { Container } from "@mui/material";
 import Header from "../components/Header";
 import AddTodo from "../components/AddTodo";
 import { useState } from "react";
+import axios from "axios";
 
 interface ITodoType {
   task: string;
@@ -10,10 +11,21 @@ interface ITodoType {
   owner?: string; //! bu alan zorunlu değil, eğer varsa type string olur.
 }
 
+const url = "https://634ac3fc5df952851418480f.mockapi.io/api/todos"
+
 const Main = () => {
   // const [todos, setTodos] = useState([] as ITodoType[]);
   // const [todos, setTodos] = useState<Array<ITodoType>>([]);
   const [todos, setTodos] = useState<ITodoType[]>([]); //!common use
+
+  const getTodos = async()=>{
+    try {
+      const res = await axios(url)
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <Container>
       <Header />
