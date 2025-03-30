9e3056e2c63ffe02f75aacd39ef5713d9851a95a
ozkan <ozkandnz09@gmail.com>
2025-03-30 13:17:18 +0300

    use interface

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -3,18 +3,19 @@ import Header from "../components/Header";
 import AddTodo from "../components/AddTodo";
 import { useState } from "react";
 
-interface ITodoType{
-task:string,
-isDone:boolean,
-id:string | number
+interface ITodoType {
+  task: string;
+  isDone: boolean;
+  id: string | number;
+  owner?: string; //! bu alan zorunlu değil, eğer varsa type string olur.
 }
 
 const Main = () => {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([] as ITodoType[]);
   return (
     <Container>
       <Header />
-      <AddTodo/>
+      <AddTodo />
     </Container>
   );
 };
