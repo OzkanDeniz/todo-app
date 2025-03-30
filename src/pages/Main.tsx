383c742e2542f74ed1b0a2d30275e8d1d0eb4a76
ozkan <ozkandnz09@gmail.com>
2025-03-30 13:14:09 +0300

    interface using when object

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,8 +1,16 @@
 import { Container } from "@mui/material";
 import Header from "../components/Header";
 import AddTodo from "../components/AddTodo";
+import { useState } from "react";
+
+interface ITodoType{
+task:string,
+isDone:boolean,
+id:string | number
+}
 
 const Main = () => {
+  const [todos, setTodos] = useState([])
   return (
     <Container>
       <Header />
