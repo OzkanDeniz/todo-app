5c6cf3adfb84623fa20aad1e5d0094f22f0d921b
ozkan <ozkandnz09@gmail.com>
2025-04-04 15:30:52 +0300

    export

--- src/types.d.ts
+++ src/types.d.ts
@@ -4,3 +4,5 @@ interface ITodoType {
     id: string | number;
     owner?: string; //! bu alan zorunlu değil, optional. Eğer varsa type string olur. Objelerde interface kullanılır.
   }
+
+//   export etmemize sağlayan sayfa
