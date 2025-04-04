89428ab19607ccf5d89452378d4ff0c0dc60ff55
ozkan <ozkandnz09@gmail.com>
2025-04-04 16:09:04 +0300

    FC

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,5 +1,13 @@
 import { Grid, Typography } from "@mui/material";
 
+//* 3.yol React.FC kullanımı
+//! React.FC, bir functional component'i ifade eder. Bileşenin işlevsel bir yapıda olduğunu ve React tarafından kullanılabileceğini belirtir.
+
+//? React.FC, bileşenlere props tanımlamak ve JSX döndürmek için kullanılan bir genel tip (generic type) olarak kullanılır. Bu tip, bileşenin alacağı props'ların tipini belirtmek için kullanılır. Props'ların tipini belirtmek, bileşenin daha güvenli ve hatasız olmasını sağlar.
+
+//* Ancak, React.FC kullanmak zorunlu değildir. Fonksiyonel bir bileşen, sadece fonksiyon olarak da tanımlanabilir ve React.FC kullanmadan da props alabilir ve JSX döndürebilir.
+
+//+ FC, "FunctionComponent" kelimelerinin kısaltmasıdır.
 
 interface ITodoList{
     todos:ITodoType[]
