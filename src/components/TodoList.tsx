1dec855853da414e7dc6cca7c7e7178d19f47723
ozkan <ozkandnz09@gmail.com>
2025-04-04 14:08:56 +0300

    todolist

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,72 @@
+import { Grid, Typography } from "@mui/material";
+import "./style.css";
+
+const TodoList = () => {
+  return (
+    <Grid
+      container
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "0.5rem",
+        mt: 3,
+      }}
+    >
+      <Grid
+        size={{ xs: 12, sm: 8, md: 5 }}
+        position="relative"
+        className="myscrool scrool-progress"
+        sx={{
+          minHeight: "350px",
+          maxHeight: "350px",
+          overflow: "auto",
+          border: "1px solid purple",
+          borderRadius: "0.5rem",
+        }}
+      >
+        <Typography
+          className="title"
+          color="secondary"
+          align="center"
+          variant="h4"
+        >
+          InProgress Todos
+        </Typography>
+        (
+        <Typography color="error" mt={3}>
+          No InProgress Todos!
+        </Typography>
+        )
+      </Grid>
+      <Grid
+        size={{ xs: 12, sm: 8, md: 5 }}
+        position="relative"
+        className="myscrool scrool-progress"
+        sx={{
+          minHeight: "350px",
+          maxHeight: "350px",
+          overflow: "auto",
+          border: "1px solid purple",
+          borderRadius: "0.5rem",
+        }}
+      >
+        <Typography
+          className="title"
+          sx={{ color: "green" }}
+          align="center"
+          variant="h4"
+        >
+          Completed Todos
+        </Typography>
+        (
+        <Typography color="error" mt={3}>
+          No Completed Todos!
+        </Typography>
+        )
+      </Grid>
+    </Grid>
+  );
+};
+
+export default TodoList;
