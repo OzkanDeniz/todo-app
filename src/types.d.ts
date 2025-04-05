d3b943b233443dcbb79a5b59f9558ef399184d10
ozkan <ozkandnz09@gmail.com>
2025-04-05 11:17:52 +0300

    deleteFn

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,11 +1,12 @@
 interface ITodoType {
-    task: string;
-    isDone: boolean;
-    id: string | number;
-    owner?: string; //! bu alan zorunlu değil, optional. Eğer varsa type string olur. Objelerde interface kullanılır.
-  }
+  task: string;
+  isDone: boolean;
+  id: string | number;
+  owner?: string; //! bu alan zorunlu değil, optional. Eğer varsa type string olur. Objelerde interface kullanılır.
+}
 
 //   export etmemize sağlayan sayfa
 
-type AddFn = (task: string) => Promise<void>
-type ToggleFn = (todo: ITodoType) => Promise<void>
+type AddFn = (task: string) => Promise<void>;
+type ToggleFn = (todo: ITodoType) => Promise<void>;
+type deleteFn = (id: string | number) => Promise<void>;
